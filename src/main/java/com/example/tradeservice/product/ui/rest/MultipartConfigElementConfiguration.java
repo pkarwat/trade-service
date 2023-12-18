35420a528480e257b486686b378a1284b26c881c
patrykkarwat <karwat.patryk@gmail.com>
2023-12-18 21:53:02 +0100

    support large trades sets

---
+++ src/main/java/com/example/tradeservice/product/ui/rest/MultipartConfigElementConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.tradeservice.product.ui.rest;
+
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+@Configuration
+class MultipartConfigElementConfiguration {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.ofMegabytes(100));
+        factory.setMaxRequestSize(DataSize.ofMegabytes(100));
+        return factory.createMultipartConfig();
+    }
+}
